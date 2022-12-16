92ac6bbe347ec827c03270632729ae53874e3a33
Иван Искусков <Iskuskov@yandex.ru>
2022-12-16 21:53:43 +0500

    test

---
+++ lesson5DZ2.py
@@ -0,0 +1,30 @@
+from random import randint
+
+
+konf = 2021
+print('Игра "Конфеты". В вазе 2021 конфета, за раз вы можете взять не более 28 конфет, ')
+print('выигрывает тот, кто возьмет последнюю конфету')
+while konf >= 0:
+    man = int(input('Сколько конфет возьмешь? '))
+    if man > 28 or man > konf:
+        print('Столько брать нельзя, начни сначала')
+        break
+    else:    
+        konf -= man
+        if konf == 0:
+            print('Поздравляю! Вы выиграли!')
+            break
+
+    if konf >= 28:
+        bot = randint(1, 29)
+        print(f'Бот взял конфет: {bot}')
+        konf -= bot
+        print(f'Осталось конфет: {konf}')
+    elif konf <= 28:
+        bot = randint(1, konf)
+        print(f'Бот взял конфет: {bot} ')
+        konf -= bot
+        print(f'Осталось конфет: {konf}')
+    if konf == 0:
+        print('Вы проиграли!')
+        break
---
+++ lesson5DZ5.py
@@ -0,0 +1,23 @@
+nums = [3, 1, 2, 3, 4, 6, 1, 7]
+
+# Первый вариант
+
+def get_up2(nums):
+    ups = [nums[0]]
+    for i in nums:
+        if i > max(ups):
+            ups.append(i)
+    return ups
+    
+print(get_up2(nums))
+
+# Второй вариант
+
+def get_up(nums):
+    ups = []
+    for i in range(len(nums)):
+        if nums[i] == max(nums[:i+1:]) and nums[i] not in ups:
+            ups.append(nums[i])
+    return ups
+
+print(get_up(nums))
